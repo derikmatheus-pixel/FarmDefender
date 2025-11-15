a821c92f9b373d8e15ef40a3fcb67ef21e6d92cc
Dkz98 <derikmatheus@gmail.com>
2025-11-15 17:07:58 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,21 @@
-// --- Configuração Firebase ---
+// firebase.js
+
 const firebaseConfig = {
-  apiKey: "AIzaSyA_Cgh9z56sQaMof3e86NSNmKuwF1a9DfM",
-  authDomain: "farmdefender-5e7ae.firebaseapp.com",
-  databaseURL: "https://farmdefender-5e7ae-default-rtdb.firebaseio.com",
-  projectId: "farmdefender-5e7ae",
-  storageBucket: "farmdefender-5e7ae.firebasestorage.app",
-  messagingSenderId: "513357885346",
-  appId: "1:513357885346:web:3a22bf7d9a6f39c3d18cfb"
+    apiKey: "SUA_API_KEY",
+    authDomain: "SEU_PROJETO.firebaseapp.com",
+    databaseURL: "https://SEU_PROJETO-default-rtdb.firebaseio.com",
+    projectId: "SEU_PROJETO",
+    storageBucket: "SEU_PROJETO.appspot.com",
+    messagingSenderId: "XXXXXXX",
+    appId: "X:XXXXXXX:web:XXXXXXXX"
 };
 
-// 2) Inicializa app e database
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
-// 3) Função para salvar pontuação
 function salvarPontuacao(nome, pontos) {
     const ref = db.ref("ranking");
     const novaRef = ref.push();
-
     return novaRef.set({
         nome: nome,
         pontos: Number(pontos) || 0,
@@ -25,21 +23,16 @@ function salvarPontuacao(nome, pontos) {
     });
 }
 
-// 4) Função para buscar ranking (top 10)
 function carregarRanking(callback) {
     const ref = db.ref("ranking").orderByChild("pontos").limitToLast(10);
 
     ref.on("value", snapshot => {
         const dados = snapshot.val() || {};
         const lista = Object.values(dados);
-
-        // ordena desc (maior pontuação primeiro)
         lista.sort((a, b) => b.pontos - a.pontos);
-
         callback(lista);
     });
 }
 
-// 5) Disponibiliza no escopo global
 window.salvarPontuacao = salvarPontuacao;
 window.carregarRanking = carregarRanking;
