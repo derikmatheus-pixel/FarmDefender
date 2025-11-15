d75c318e7758bc41d1dd5d739929f7d043a455fa
Dkz98 <derikmatheus@gmail.com>
2025-11-15 19:24:48 -0300

    Update game-events.js

--- game-events.js
+++ game-events.js
@@ -2,10 +2,8 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     const btnLogout = document.getElementById("btnLogout");
-
     if (btnLogout) {
         btnLogout.addEventListener("click", function () {
-            // se quiser voltar para o menu/login
             window.location.href = "index.html";
         });
     }
