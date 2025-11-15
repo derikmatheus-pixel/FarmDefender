5144f036e269ceeb58dca01d71714a722dedd052
Dkz98 <derikmatheus@gmail.com>
2025-11-15 17:28:54 -0300

    Create game-enemies.js

---
+++ game-enemies.js
@@ -0,0 +1,41 @@
+function spawnCorvo() {
+    if (!rodando) return;
+
+    const cenario = document.getElementById("cenario");
+    const corvo = document.createElement("img");
+
+    corvo.src = "corvo.png";
+    corvo.classList.add("corvo");
+
+    corvo.style.width = "55px";
+    corvo.style.top = "-70px";
+    corvo.style.left = Math.random() * (cenario.clientWidth - 60) + "px";
+
+    corvo.onclick = () => matarCorvo(corvo);
+
+    cenario.appendChild(corvo);
+    corvos.push(corvo);
+}
+
+function moverCorvos() {
+    const chao = 300;
+
+    corvos.forEach((corvo, i) => {
+        let y = parseInt(corvo.style.top);
+        corvo.style.top = (y + 2) + "px";
+
+        if (y >= chao) {
+            corvo.remove();
+            corvos.splice(i, 1);
+            perderVida();
+        }
+    });
+}
+
+function matarCorvo(corvo) {
+    pontos++;
+    atualizarHUD();
+
+    corvos = corvos.filter(c => c !== corvo);
+    corvo.remove();
+}
