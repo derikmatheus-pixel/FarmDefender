611d4e59bc342a9f53a905c0c7e2a27e6a69722e
Dkz98 <derikmatheus@gmail.com>
2025-11-15 17:28:10 -0300

    Create game-core.js

---
+++ game-core.js
@@ -0,0 +1,27 @@
+let jogador = localStorage.getItem("usuarioLogadoFarm");
+if (!jogador) window.location.href = "index.html";
+
+let pontos = 0;
+let vidas = 3;
+let rodando = true;
+
+let corvos = [];
+let fazendeiro = null;
+let spawnInterval = null;
+
+function perderVida() {
+    vidas--;
+    atualizarHUD();
+    if (vidas <= 0) finalizarJogo();
+}
+
+function finalizarJogo() {
+    rodando = false;
+    clearInterval(spawnInterval);
+
+    corvos.forEach(c => c.remove());
+    corvos = [];
+
+    salvarPontuacao(jogador, pontos)
+        .finally(() => window.location.href = "ranking.html");
+}
