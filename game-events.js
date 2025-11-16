f58e454ab051a32306de1dc2a8ec466894aa5558
Dkz98 <derikmatheus@gmail.com>
2025-11-15 23:16:08 -0300

    Update game-events.js

--- game-events.js
+++ game-events.js
@@ -3,20 +3,6 @@
 // ==========================
 
 window.GameEvents = {
-    // Corvo entra na linha que tem fazendeiro?
-    verificarDefesaLinha: function (criatura, indiceCriatura) {
-        const s = Game.state;
-
-        const fazendeiro = s.fazendeiros.find(f => f.linha === criatura.linha);
-        if (!fazendeiro) return;
-
-        // Quando o corvo passar do alcanceX, ele é “segurado” pelo fazendeiro
-        if (criatura.x <= fazendeiro.alcanceX) {
-            s.pontos += 10; // pontos por inimigo defendido
-            GameHud.atualizarHUD();
-            GameEnemies.removerCriatura(indiceCriatura);
-        }
-    },
 
     // Corvo passou totalmente pela borda esquerda
     corvoPassouLinha: function (indiceCriatura) {
